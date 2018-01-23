f3372c2742ff6187770d84503a21a3c9c3abca95
Shubham Singh <coolsks94@gmail.com>
2018-01-23 18:25:35 +0530

    Option to use accent color for pulse [1/3]
    Change-Id: I8fb41eb3ed9cf81f0153dbb9871c33db0d42ba23
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- src/com/android/systemui/navigation/pulse/FadingBlockRenderer.java
+++ src/com/android/systemui/navigation/pulse/FadingBlockRenderer.java
@@ -51,6 +51,7 @@ public class FadingBlockRenderer extends Renderer implements ColorAnimator.Color
     private int dbValue;
     private float magnitude;
     private int mDivisions;
+    private int mAccentColor;
     private int mUserColor;
     private int mAlbumColor = -1;
     private boolean mAutoColor;
@@ -66,6 +67,7 @@ public class FadingBlockRenderer extends Renderer implements ColorAnimator.Color
 
     private ColorAnimator mLavaLamp;
     private LegacySettingsObserver mObserver;
+    private boolean mPulseAccentColorEnabled;
     private boolean mLavaLampEnabled;
     private boolean mIsValidStream;
 
@@ -94,7 +96,7 @@ public class FadingBlockRenderer extends Renderer implements ColorAnimator.Color
         mIsValidStream = isValid;
         if (isValid) {
             onSizeChanged(0, 0, 0, 0);
-            if (mLavaLampEnabled) {
+            if (mLavaLampEnabled  && !mPulseAccentColorEnabled) {
                 mLavaLamp.start();
             }
         }
@@ -147,7 +149,7 @@ public class FadingBlockRenderer extends Renderer implements ColorAnimator.Color
 
     @Override
     public void onColorChanged(ColorAnimator colorAnimator, int color) {
-        mPaint.setColor(color);
+        mPaint.setColor(mPulseAccentColorEnabled ? mAccentColor : color);
     }
 
     @Override
@@ -156,7 +158,7 @@ public class FadingBlockRenderer extends Renderer implements ColorAnimator.Color
 
     @Override
     public void onStopAnimation(ColorAnimator colorAnimator, int lastColor) {
-        mPaint.setColor(mAutoColor && mAlbumColor != -1 ? mAlbumColor : mUserColor);
+        mPaint.setColor(mAutoColor && mAlbumColor != -1 ? mAlbumColor : (mPulseAccentColorEnabled ? mAccentColor : mUserColor));
     }
 
     @Override
@@ -200,6 +202,10 @@ public class FadingBlockRenderer extends Renderer implements ColorAnimator.Color
 
         void register() {
             ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(
+                    Settings.Secure.getUriFor(Settings.Secure.PULSE_ACCENT_COLOR_ENABLED), false,
+                    this,
+                    UserHandle.USER_ALL);
             resolver.registerContentObserver(
                     Settings.Secure.getUriFor(Settings.Secure.FLING_PULSE_COLOR), false, this,
                     UserHandle.USER_ALL);
@@ -247,6 +253,9 @@ public class FadingBlockRenderer extends Renderer implements ColorAnimator.Color
                     resolver, Settings.Secure.PULSE_AUTO_COLOR, 0,
                     UserHandle.USER_CURRENT) == 1;
 
+            mPulseAccentColorEnabled = Settings.Secure.getIntForUser(resolver,
+                    Settings.Secure.PULSE_ACCENT_COLOR_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
+
             mLavaLampEnabled = !mAutoColor && Settings.Secure.getIntForUser(resolver,
                     Settings.Secure.FLING_PULSE_LAVALAMP_ENABLED, 1, UserHandle.USER_CURRENT) == 1;
 
@@ -254,7 +263,13 @@ public class FadingBlockRenderer extends Renderer implements ColorAnimator.Color
                     Settings.Secure.FLING_PULSE_COLOR,
                     mContext.getResources().getColor(R.color.config_pulseFillColor),
                     UserHandle.USER_CURRENT);
-            if (!mLavaLampEnabled) {
+            mAccentColor = mContext.getResources().getColor(R.color.pulseAccentColor);
+
+            if (mPulseAccentColorEnabled) {
+                mPaint.setColor(mAccentColor);
+            }
+
+            if (!mLavaLampEnabled && !mPulseAccentColorEnabled) {
                 int lastColor = mController.getAlbumArtColor();
                 mPaint.setColor(mAutoColor && lastColor != -1 ? lastColor : mUserColor);
             }
@@ -262,7 +277,7 @@ public class FadingBlockRenderer extends Renderer implements ColorAnimator.Color
                     Settings.Secure.FLING_PULSE_LAVALAMP_SPEED, 10000,
                     UserHandle.USER_CURRENT);
             mLavaLamp.setAnimationTime(time);
-            if (mLavaLampEnabled && mIsValidStream) {
+            if (mLavaLampEnabled && mIsValidStream && !mPulseAccentColorEnabled) {
                 mLavaLamp.start();
             } else {
                 mLavaLamp.stop();
