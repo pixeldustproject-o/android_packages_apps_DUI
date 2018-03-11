be931c5aa88049b935a61cbafbff345856ef3c59
ezio84 <brabus84@gmail.com>
2018-03-11 19:00:20 +0100

    Fix smartbar media arrows not showing sometimes
    AudioManager isMusicActive() api is not reliable, and
    anyway we check blacklisted packages in StatusBar setMediaPlaying
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- src/com/android/systemui/navigation/smartbar/SmartBarView.java
+++ src/com/android/systemui/navigation/smartbar/SmartBarView.java
@@ -396,7 +396,7 @@ public class SmartBarView extends BaseNavigationBar {
     }
 
     private void setMediaArrowsVisibility(boolean backAlt) {
-        setMediaArrowsVisibility(mCurrentView, (!backAlt && (mIsMediaPlaying&& mAudioManager.isMusicActive()))
+        setMediaArrowsVisibility(mCurrentView, (!backAlt && (mIsMediaPlaying /*&& mAudioManager.isMusicActive()*/))
                 ? View.VISIBLE : View.INVISIBLE);
     }
 
